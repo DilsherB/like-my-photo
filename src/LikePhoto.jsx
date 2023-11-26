d4acd479145d39779420914edc594eb6286bf2df
Dilsher <dilsherb@gmail.com>
2023-11-26 14:44:43 +0300

    like my photo project completed.

--- src/LikePhoto.jsx
+++ src/LikePhoto.jsx
@@ -1,13 +1,41 @@
-import React from 'react'
-import Title from './components/Title'
+import React, { useState } from "react";
+import Title from "./components/Title";
+import { FaRegCommentDots } from "react-icons/fa6";
+import { FcDislike, FcLike } from "react-icons/fc";
 
 const LikePhoto = () => {
+  const [like, setLike] = useState(false);
+  const [count, setCount] = useState(0);
+  const handleLike = () => {
+    setLike(!like);
+    like ? setCount((prev) => prev - 1) : setCount((prev) => prev + 1);
+    // if (like) {
+    //   setCount(count - 1);
+    // } else {
+    //   setCount(count + 1);
+    // }
+  };
   return (
     <>
       <Title classes={"title"} text={"Like My Photo"} />
-      <Title classes={"subTitle"} text={"Likes"} />
+      <div className="card">
+        <div className="likes">
+          <Title classes={"subTitle"} text={"Likes"} />
+          {count}
+        </div>
+        <img
+          src="https://picsum.photos/200/300?image=100"
+          alt="Avatar"
+          style={{ width: "100%" }}
+          onDoubleClick={handleLike}
+        />
+        <div className="container">
+          <FaRegCommentDots />
+          <div onClick={handleLike}>{like ? <FcLike /> : <FcDislike />}</div>
+        </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default LikePhoto
+export default LikePhoto;
